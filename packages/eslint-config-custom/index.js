b79366ff498485b5aba480a81c71632daad1573a
Alex Ilushyk <ailushyk@gmail.com>
2023-04-09 22:45:27 +0200

    upgrade deps, fixed eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,17 @@ module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    "react/jsx-curly-brace-presence": [
+      "error",
+      { props: "never", children: "never" },
+    ],
   },
   parserOptions: {
     babelOptions: {
