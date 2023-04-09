11aec349d876ee0439af003de309ad6ca719016c
Alex Ilushyk <ailushyk@gmail.com>
2023-04-09 22:45:43 +0200

    use experimental app dir in docs app

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,17 +1,14 @@
-import Head from "next/head";
+"use client";
+import React from "react";
 import { Button } from "ui";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Docs - Turborepo Example</title>
-      </Head>
-
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+      <main className="mx-auto w-auto px-4 pb-8 pt-16 sm:pt-24 lg:px-8">
         <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           Docs
-          <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2">
+          <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text px-2 text-transparent">
             Turborepo Example
           </span>
         </h1>
