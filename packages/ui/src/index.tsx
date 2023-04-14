05f1821efe654363690a085699361b6ab61120a9
Alex Ilushyk <ailushyk@gmail.com>
2023-04-15 01:24:55 +0200

    feat(ui): added new components
    - List
    - Section

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,4 +4,6 @@ import './styles.css'
 // components
 export * from './components/Button'
 export * from './components/Card'
+export * from './components/List'
+export * from './components/Section'
 export * from './components/Widget'
