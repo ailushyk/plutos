72c55baa6f7f81f70d2f529faa7d1aa19069feb6
Alex Ilushyk <ailushyk@gmail.com>
2023-04-14 06:52:17 +0200

    feat: experimental mode appDir enabled for web

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/globals.css";
+// include styles from the ui package
+import "ui/styles.css";
+import { Inter } from "next/font/google";
+
+export const metadata = {
+  title: "Plutos",
+  description: "",
+};
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${inter.variable} bg-zinc-900`}>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
