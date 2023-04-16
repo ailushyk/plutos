20cf84e395dcd3a184f6b422f869639e009ca39f
Alex Ilushyk <ailushyk@gmail.com>
2023-04-16 13:05:28 +0200

    chore(web): use the Inter font with Next.js

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,18 @@
 // tailwind config is required for editor support
-
-const sharedConfig = require("tailwind-config/tailwind.config.js");
+const sharedConfig = require('tailwind-config/tailwind.config.js')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   presets: [sharedConfig],
-};
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          // 'Inter var',
+          'var(--font-inter)',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+}
