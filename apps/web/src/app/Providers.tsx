79ff0feffa29de6722d689094cae1255872287de
Alex I <ailushyk@gmail.com>
2023-05-06 21:04:47 +0200

    wip: update clerk

--- apps/web/src/app/Providers.tsx
+++
@@ -1,7 +0,0 @@
-import { ClerkProvider } from '@clerk/nextjs/app-beta'
-
-function Providers({ children }) {
-  return <ClerkProvider>{children}</ClerkProvider>
-}
-
-export default Providers
--- apps/web/src/app/[lang]/layout.tsx
+++ apps/web/src/app/[lang]/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import '../../styles/globals.css'
-// include styles from the ui package
-import 'ui/styles.css'
 import { Inter } from 'next/font/google'
-import Providers from '@/app/Providers'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import '../../styles/globals.css'
+import 'ui/styles.css'
 
 export const metadata = {
   title: 'Plutos',
@@ -20,14 +20,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html
-      lang={params.lang}
-      className={`${inter.variable} bg-zinc-950 text-white/90`}
-    >
-      <body className="antialiased">
-        <div className="status-bar h-12" />
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang={params.lang}
+        className={`${inter.variable} bg-zinc-950 text-white/90`}
+      >
+        <body className="antialiased">
+          <div className="status-bar h-12" />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
