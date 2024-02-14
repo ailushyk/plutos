db08e0c7902cd378fd2bcc7deb803f4c6cca6517
Alex I <ailushyk@gmail.com>
2024-02-14 10:49:30 +0100

    chore: the home page is conditionally disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { Poppins } from 'next/font/google'
+import { appConfig } from '@/app-config'
 
 import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { LoginButton } from '@/components/auth/login-button'
 
 const font = Poppins({ subsets: ['latin'], weight: ['600'] })
 
@@ -16,9 +19,32 @@ export default function Home() {
       <h1
         className={cn('text-6xl font-semibold drop-shadow-md', font.className)}
       >
-        Plutos
+        {appConfig.appName}
       </h1>
-      <p className="text-lg drop-shadow-md">Coming soon...</p>
+
+      {process.env.FF_ENABLE_HOME_PAGE_SIGN_IN ? (
+        <FF__SignInForm />
+      ) : (
+        <FF__ComingSoon />
+      )}
     </main>
+  )
+}
+
+function FF__ComingSoon() {
+  return <p className="text-lg drop-shadow-md">Coming soon...</p>
+}
+
+function FF__SignInForm() {
+  return (
+    <>
+      <p className="text-center text-lg drop-shadow-md">
+        A new way to manage your finances
+      </p>
+
+      <LoginButton asChild>
+        <Button size="lg">Sigh In</Button>
+      </LoginButton>
+    </>
   )
 }
