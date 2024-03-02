a880c6dadd53071b4e3b0702fe8ab27a9836b927
Alex I <ailushyk@gmail.com>
2024-03-02 22:24:08 +0100

    chore: mobile navigation has larger buttons

--- src/components/nav/mobile-nav.tsx
+++ src/components/nav/mobile-nav.tsx
@@ -13,7 +13,7 @@ export function MobileMenua(props: { children: ReactNode }) {
 
 export function MobileMenu(props: { children: ReactNode }) {
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex justify-center gap-5 border-t bg-background/50 p-3 backdrop-blur-md md:hidden">
+    <div className="fixed bottom-0 left-0 right-0 flex justify-center gap-4 border-t bg-background/50 p-3 backdrop-blur-md md:hidden">
       {props.children}
     </div>
   )
@@ -27,7 +27,7 @@ export function MobileMenuItem({
   children: React.ReactNode
 }) {
   return (
-    <Button asChild variant="outline" size="icon">
+    <Button asChild variant="outline" className="h-11 w-11 p-0">
       <Link href={href}>{children}</Link>
     </Button>
   )
